dd81061f5f3c3c8e7f194c7846c1e899f3f96504
Eugene <eugene.code@yandex.ru>
2022-11-08 11:53:17 +0300

    Moved Managers from partial view to the separate component
    Renamed Items to _managers in the Manager component

--- LedIce/Pages/Contacts.cshtml.cs
+++ LedIce/Pages/Contacts.cshtml.cs
@@ -13,29 +13,25 @@ public sealed class ContactsModel : PageModel, ISeoable
     private readonly LinkGenerator _linkGenerator;
     private readonly PageMetaService _pageMetaService;
     private readonly LocationService _locationService;
-    private readonly SocialService _socialService;
+    
 
     public ContactsModel(
         LinkGenerator linkGenerator, 
         PageMetaService pageMetaService, 
         LocationService service,
-        SocialService socialService,
         IStringLocalizer<ContactsModel> text)
     {
         _linkGenerator = linkGenerator;
         _pageMetaService = pageMetaService;
         _locationService = service;
-        _socialService = socialService;
 
         Text = text;
         PageMeta = default!;
         Location = default!;
-        Socials = default!;
     }
 
     public PageMeta PageMeta { get; private set; }
     public Location Location { get; private set; }
-    public IEnumerable<Social?> Socials { get; private set; }
     public IStringLocalizer<ContactsModel> Text { get; private init; }
     public string Seo { get; init; } = "contacts";
 
@@ -43,7 +39,6 @@ public sealed class ContactsModel : PageModel, ISeoable
     {
         PageMeta = await _pageMetaService.GetPageMetaAsync(this) ?? new();
         Location = await _locationService.GetFirstLocationAsync() ?? new();
-        Socials = await _socialService.GetSocialsAsync() ?? Enumerable.Empty<Social>();
 
         InitializeViewData();
     }
---
+++ LedIce/Pages/Shared/Managers.razor.cs
@@ -0,0 +1,19 @@
+﻿using LedIce.DTO;
+using LedIce.Services;
+
+using Microsoft.AspNetCore.Components;
+
+namespace LedIce.Pages.Shared;
+
+public partial class Managers
+{
+    private IEnumerable<Manager> _managers = Enumerable.Empty<Manager>();
+
+    [Inject]
+    private ManagerService Service { get; init; } = default!;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        _managers = await Service.GetManagersAsync();
+    }
+}
