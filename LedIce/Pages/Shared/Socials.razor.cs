e3b69d050b65ed702c5c65c5514134259e4bcafb
Eugene <eugene.code@yandex.ru>
2022-11-08 13:17:48 +0300

    Moved Socials from partial view to the separate component

---
+++ LedIce/Pages/Shared/Socials.razor.cs
@@ -0,0 +1,19 @@
+﻿using LedIce.DTO;
+using LedIce.Services;
+
+using Microsoft.AspNetCore.Components;
+
+namespace LedIce.Pages.Shared;
+
+public partial class Socials
+{
+    private IEnumerable<Social> _socials = Enumerable.Empty<Social>();
+
+    [Inject]
+    private SocialService Service { get; init; } = default!;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        _socials = await Service.GetSocialsAsync();
+    }
+}
