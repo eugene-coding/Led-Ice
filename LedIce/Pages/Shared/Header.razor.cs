a5eb92a09485a2079e54a348e4345dc3a4131355
Eugene <eugene.code@yandex.ru>
2022-11-08 16:39:03 +0300

    Refactored layouts

---
+++ LedIce/Pages/Shared/Header.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LedIce.Pages.Shared;
+
+public partial class Header
+{
+    [Parameter]
+    public bool Fixed { get; init; }
+
+    private string? Class => Fixed ? "header_fixed" : null;
+}
